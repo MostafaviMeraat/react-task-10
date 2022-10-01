05e2d747f02fd216fbd496438d5f029a22fef69d
Meraat.Mostafavi <meraat.mostafavi@gmail.com>
2022-10-02 02:43:19 +0330

    unauthorize personel access denied

---
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ProtectedRoute = () => {
+
+  return (
+    <div>
+      < h1 style={{ textAlign: 'center' }} > 'You are not ADMIN !!!'</h1>
+    </div>
+
+  )
+}
+
+export default ProtectedRoute
